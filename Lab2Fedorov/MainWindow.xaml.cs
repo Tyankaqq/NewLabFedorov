67ef841a02f674533f1532cd5bfe72bb23d09dcb
Tyankaqq <Tyankaqq@outlook.com>
2024-01-29 16:28:43 +0200

    hhahahah

---
+++ Lab2Fedorov/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using Lab2Fedorov.model;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lab2Fedorov
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private double RateDollar = 89.52;
+        private double RateEuro = 97.09;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (DollarVariant.IsChecked == true)
+            {
+                Dollar dollar = new Dollar(int.Parse(SecondSize.Text), RateDollar);
+                ResFirst.Text = dollar.Trade().ToString();
+            }
+            else
+            {
+                Euro euro = new Euro(int.Parse(SecondSize.Text), RateEuro);
+                ResFirst.Text = euro.Trade().ToString();
+            }
+        }
+    }
+}
---
+++ Lab2Fedorov/model/Dollar.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2Fedorov.model
+{
+    internal class Dollar : ICurrency
+    {
+        public int quality { get; set; }
+        public double rate { get; set; }
+        public Dollar(int quality, double rate)
+        {
+            this.quality = quality;
+            this.rate = rate;
+        }
+        public double Trade()
+        {
+            double result = quality * rate;
+            Console.WriteLine("Курс в рублях" + result);
+            return result;
+        }
+        
+    }
+}
