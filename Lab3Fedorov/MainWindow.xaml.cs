befbbe3af25fd817c80265680a5bdf020dee132a
Tyankaqq <Tyankaqq@outlook.com>
2024-02-19 11:16:44 +0200

    hhshshs

---
+++ Lab3Fedorov/MainWindow.xaml.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lab3Fedorov
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+            public MainWindow()
+            {
+                InitializeComponent();
+            }
+            private void Button_Click(object sender, RoutedEventArgs e)
+            {
+                Money m1 = new Money()
+                {
+                    Rubles = long.Parse(FirstOne.Text),
+                    Cent = byte.Parse(FirstTwo.Text)
+                };
+                Money m2 = new Money()
+                {
+                    Rubles = long.Parse(SecondOne.Text),
+                    Cent = byte.Parse(SecondTwo.Text)
+                };
+                Money res = m1 + m2;
+                Result.Text = res.ToString();
+            }
+
+            private void Button_Click_1(object sender, RoutedEventArgs e)
+            {
+                Money m1 = new Money()
+                {
+                    Rubles = long.Parse(FirstOne.Text),
+                    Cent = byte.Parse(FirstTwo.Text)
+                };
+                Money m2 = new Money()
+                {
+                    Rubles = long.Parse(SecondOne.Text),
+                    Cent = byte.Parse(SecondTwo.Text)
+                };
+                Money res = m1 - m2;
+                Result.Text = res.ToString();
+            }
+
+            private void Button_Click_2(object sender, RoutedEventArgs e)
+            {
+                Money m1 = new Money()
+                {
+                    Rubles = long.Parse(FirstOne.Text),
+                    Cent = byte.Parse(FirstTwo.Text)
+                };
+                Money m2 = new Money()
+                {
+                    Rubles = long.Parse(SecondOne.Text),
+                    Cent = byte.Parse(SecondTwo.Text)
+                };
+                Money res = m1 * m2;
+                Result.Text = res.ToString();
+            }
+
+            private void Button_Click_3(object sender, RoutedEventArgs e)
+            {
+                Money m1 = new Money()
+                {
+                    Rubles = long.Parse(FirstOne.Text),
+                    Cent = byte.Parse(FirstTwo.Text)
+                };
+                Money m2 = new Money()
+                {
+                    Rubles = long.Parse(SecondOne.Text),
+                    Cent = byte.Parse(SecondTwo.Text)
+                };
+                Result.Text = (m1 == m2).ToString();
+            }
+        }
+}
+
---
+++ Lab3Fedorov/Money.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.RightsManagement;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Lab3Fedorov
+{
+    class Money
+    {
+        public long Rubles { get; set; }
+        public byte Cent { get; set; }
+    //    private byte cent; 
+
+
+    //public byte сent
+    //{
+    //    get { return cent; }
+    //    set
+    //    {
+    //        if (value >= 0 && value <= 99) 
+    //        {
+    //            cent = value;
+    //        }
+    //        else
+    //        {
+    //            throw new ArgumentOutOfRangeException("Cent должен быть в диапазоне от 0 до 99.");
+    //        }
+    //    }
+    //}
+
+        public static Money operator +(Money m1, Money m2)
+        {
+            double a1 = m1.Rubles + (m1.Cent / Math.Pow(10, m1.Cent.ToString().Length));
+            double a2 = m2.Rubles + (m2.Cent / Math.Pow(10, m2.Cent.ToString().Length));
+            double res = Math.Round(a1 + a2, 3);
+            string[] parts = res.ToString().Split(',');
+            byte cents = 0;
+            if (parts.Length > 1)
+            {
+                byte.TryParse(parts[1], out cents);
+            }
+
+            return new Money() { Rubles = (long)Math.Floor(res), Cent = cents };
+        }
+
+        public static Money operator -(Money m1, Money m2)
+        {
+            double a1 = m1.Rubles + (m1.Cent / Math.Pow(10, m1.Cent.ToString().Length));
+            double a2 = m2.Rubles + (m2.Cent / Math.Pow(10, m2.Cent.ToString().Length));
+            double res = Math.Round(a1 - a2, 3);
+            string[] parts = res.ToString().Split(',');
+            byte cents = 0;
+            if (parts.Length > 1)
+            {
+                byte.TryParse(parts[1], out cents);
+            }
+
+            return new Money() { Rubles = (long)Math.Floor(res), Cent = cents };
+
+        }
+
+        public static Money operator *(Money m1, Money m2)
+        {
+            double a1 = m1.Rubles + (m1.Cent / Math.Pow(10, m1.Cent.ToString().Length));
+            double a2 = m2.Rubles + (m2.Cent / Math.Pow(10, m2.Cent.ToString().Length));
+            double res = Math.Round(a1 * a2, 3);
+            string[] parts = res.ToString().Split(',');
+            byte cents = 0;
+            if (parts.Length > 1)
+            {
+                byte.TryParse(parts[1], out cents);
+            }
+
+            return new Money() { Rubles = (long)Math.Floor(res), Cent = cents };
+        }
+
+        public static Money operator /(Money m1, Money m2)
+        {
+            double a1 = m1.Rubles + (m1.Cent / Math.Pow(10, m1.Cent.ToString().Length));
+            double a2 = m2.Rubles + (m2.Cent / Math.Pow(10, m2.Cent.ToString().Length));
+            double res = Math.Round(a1 / a2, 3);
+            string[] parts = res.ToString().Split(',');
+            byte cents = 0;
+            if (parts.Length > 1)
+            {
+                byte.TryParse(parts[1], out cents);
+            }
+
+            return new Money() { Rubles = (long)Math.Floor(res), Cent = cents };
+        }
+
+        public static bool operator ==(Money m1, Money m2)
+        {
+            if (m1.Rubles == m2.Rubles && m1.Cent == m2.Cent)
+                return true;
+            else return false;
+        }
+        public static bool operator !=(Money m1, Money m2)
+        {
+            if (m1.Rubles != m2.Rubles || m1.Cent != m2.Cent)
+                return true;
+            else return false;
+        }
+
+        public override string ToString()
+        {
+            return $"{Rubles},{Cent}";
+        }
+    }
+
+}
+
+
+
